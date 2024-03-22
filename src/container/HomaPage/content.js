39302d0a354e8bcebed95e5cfba8827dc36d019f
divya <divya.dixit@ongraph.com>
2024-03-22 14:04:02 +0530

    homepage images changed

--- src/container/HomaPage/content.js
+++ src/container/HomaPage/content.js
@@ -1,6 +1,6 @@
 export const HomeCardsData = [
   {
-    imageUrl: "/images/utxo2.svg",
+    imageUrl: "/images/utxo-dapp-1.jpg",
     header: "Build with and for the UTXO community!",
     subtitle1:
       "We are delighted to extend this formal invitation to you to join the UTXO Token Community on Bitcoin.",
@@ -8,7 +8,7 @@ export const HomeCardsData = [
       "As a vibrant and innovative space within the broader cryptocurrency ecosystem, the UTXO Token Community brings together individuals and entities passionate about exploring the potential and possibilities of Bitcoin's UTXO model.",
   },
   {
-    imageUrl: "/images/utxo3.svg",
+    imageUrl: "/images/utxo-dapp-2.jpg",
     header: "Discover all new potential utility that UTXO offers today!",
     subtitle1:
       "By joining our community, you will have the opportunity to: Engage with Like-Minded Individuals: ",
@@ -16,7 +16,7 @@ export const HomeCardsData = [
       "Connect with a diverse array of stakeholders, including developers, entrepreneurs, investors, and enthusiasts, who share a common interest in exploring the intersection of tokenization and the UTXO model.",
   },
   {
-    imageUrl: "/images/utxo4.svg",
+    imageUrl: "/images/utxo-dapp-3.jpg",
     header: "Let’s shape the future of UTXO together!",
     subtitle1:
       "Shape the Future: Be part of a dynamic community that is shaping the future of tokenization on the Bitcoin network. ",
